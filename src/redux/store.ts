2541c855827d34ac5c990cac42c9e603f42d77c9
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-01-12 16:02:09 +0600

    RTK Query setup and authApi

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit/react';
 import CartReducer from './features/cartSlice';
+import { baseApi } from './api/baseApi';
 
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
     cart: CartReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
